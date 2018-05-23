5027624df02e2e762a59091be5f81d5db00e2871
Timothy Eng <timothyeng36@gmail.com>
2018-05-22 19:17:10 -0700

    Created (mostly empty) gyro-based PID program.

---
+++ GyroPID/GyroPID.ino
@@ -0,0 +1,11 @@
+/* This program turns the robot to a specified angle using PID control with gyros as feedback */
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
