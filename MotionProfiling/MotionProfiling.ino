76365e8a8d2bf5f5769c50447cbfae9d08970926
Timothy Eng <timothyeng36@gmail.com>
2018-05-25 23:54:19 -0700

    Copied over setup from EncoderPID.

--- MotionProfiling/MotionProfiling.ino
+++ MotionProfiling/MotionProfiling.ino
@@ -1,12 +1,38 @@
+/* This program drives each motor using motion profiling. */
+
 #include "Trajectory.h"
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+Zumo32U4Motors motors;
+Zumo32U4ButtonA buttonA;
+Zumo32U4Encoders encoders;
+
+Zumo32U4LCD lcd;
 
 unsigned int segIndex = 0; // Which segment we're currently on.
 
-void setup() {
-  // put your setup code here, to run once:
+/* Starting encoder values */
+int16_t startingCountsLeft = 0;
+int16_t startingCountsRight = 0;
 
+void setup() {
+  // Wait for the user to press button A.
+  lcd.clear();
+  lcd.print("Press A");
+  
+  buttonA.waitForButton();
+  
+  // Delay so that the robot does not move away while the user is
+  // still touching it.
+  delay(1000);
+  
   Serial.begin(9600);
+  
   segIndex = 0;
+
+  startingCountsLeft = encoders.getCountsLeft();
+  startingCountsRight = encoders.getCountsRight();
 }
 
 void loop() {
