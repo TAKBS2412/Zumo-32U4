db27c4ab5d900a0d655ff61ea53854cb86082e82
Timothy Eng <timothyeng36@gmail.com>
2018-05-28 16:24:03 -0700

    Print out the proximity sensor values.

--- ProximityTest/ProximityTest.ino
+++ ProximityTest/ProximityTest.ino
@@ -3,13 +3,35 @@
 #include <Wire.h>
 #include <Zumo32U4.h>
 
+Zumo32U4LCD lcd;
+Zumo32U4ButtonA buttonA;
+
 Zumo32U4ProximitySensors proxSensors;
 
 void setup() {
   proxSensors.initThreeSensors();
+
+  lcd.clear();
+  lcd.println("Press A");
+
+  buttonA.waitForButton();
+
+  lcd.clear();
+
   Serial.begin(9600);
 }
 
 void loop() {
   proxSensors.read();
+
+  static char buffer[80];
+  sprintf(buffer, "%d %d %d %d %d %d\n",
+    proxSensors.countsLeftWithLeftLeds(),
+    proxSensors.countsLeftWithRightLeds(),
+    proxSensors.countsFrontWithLeftLeds(),
+    proxSensors.countsFrontWithRightLeds(),
+    proxSensors.countsRightWithLeftLeds(),
+    proxSensors.countsRightWithRightLeds()
+  );
+  Serial.print(buffer);
 }
