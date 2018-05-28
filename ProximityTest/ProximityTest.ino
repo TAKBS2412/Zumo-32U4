8c528e870d3f3d85307b73083cac5446e4211b68
Timothy Eng <timothyeng36@gmail.com>
2018-05-28 15:43:07 -0700

    Added test proximity sensor program.

---
+++ ProximityTest/ProximityTest.ino
@@ -0,0 +1,15 @@
+/* This is a test program for reading values from the proximity sensors and detecting collisions. */
+
+#include <Wire.h>
+#include <Zumo32U4.h>
+
+Zumo32U4ProximitySensors proxSensors;
+
+void setup() {
+  proxSensors.initThreeSensors();
+  Serial.begin(9600);
+}
+
+void loop() {
+  proxSensors.read();
+}
