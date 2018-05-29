6ba6fab156849387e29224e3cccf17351ed3b1b1
Timothy Eng <timothyeng36@gmail.com>
2018-05-28 22:33:50 -0700

    Beep if the Sumobot is close to an object.

--- ProximityTest/ProximityTest.ino
+++ ProximityTest/ProximityTest.ino
@@ -7,6 +7,7 @@ Zumo32U4LCD lcd;
 Zumo32U4ButtonA buttonA;
 
 Zumo32U4ProximitySensors proxSensors;
+Zumo32U4Buzzer buzzer;
 
 void setup() {
   proxSensors.initThreeSensors();
@@ -24,6 +25,9 @@ void setup() {
 void loop() {
   proxSensors.read();
 
+  uint8_t sum = proxSensors.countsFrontWithRightLeds() + proxSensors.countsFrontWithLeftLeds();
+  int8_t diff = proxSensors.countsFrontWithRightLeds() - proxSensors.countsFrontWithLeftLeds();
+  
   static char buffer[80];
   sprintf(buffer, "%d %d %d %d %d %d\n",
     proxSensors.countsLeftWithLeftLeds(),
@@ -33,5 +37,17 @@ void loop() {
     proxSensors.countsRightWithLeftLeds(),
     proxSensors.countsRightWithRightLeds()
   );
+  
   Serial.print(buffer);
+  if (sum == 12)
+  {
+    // There's an object right in front of us, so we should ram it.
+    lcd.clear();
+    lcd.print("Ramming...");
+
+    buzzer.playNote(NOTE_G(4), 2000, 15);
+      
+    delay(200);
+    buzzer.stopPlaying();
+  }
 }
